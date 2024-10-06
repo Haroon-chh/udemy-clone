124b4fe8e8326d36e84e3e5861fb29468e0d90dc
haroon-chh <haroon4501588@gmail.com>
2024-10-06 22:24:23 +0500

    innitializing repo

---
+++ src/services/ApiServices.js
@@ -0,0 +1,39 @@
+import Vue from 'vue';
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+const baseURL = process.env.VUE_APP_API_URL;
+
+const ApiServices = {
+  init() {
+    Vue.use(VueAxios, axios);
+    axios.defaults.baseURL = baseURL;
+    axios.defaults.headers.common["Content-Type"] = "application/json";
+  },
+
+  async GetRequest(endpoint, params) {
+    try {
+      const response = await axios.get(`${baseURL}${endpoint}`, { params });
+      return response.data;
+    } catch (error) {
+      console.error('GET request failed:', error);
+      throw error;
+    }
+  },
+
+  async PostRequest(endpoint, data) {
+    try {
+      const response = await axios.post(`${baseURL}${endpoint}`, data, {
+        headers: { 
+          "Content-Type": "multipart/form-data",
+        }
+      });
+      return response.data;  // Return just the response data
+    } catch (error) {
+      console.error('POST request failed:', error);
+      throw error;
+    }
+  }
+};
+export default ApiServices;
+
---
+++ src/services/RouteGuard.js
@@ -0,0 +1,39 @@
+import store from "../store/index";
+
+const RouteGuard = (to, from, next) => {
+  const publicPages = ['/login', '/registration', '/forgot-password', '/reset-password'];
+  // const adminPages = ['/dashboard', '/view-results', '/add-staff', '/delete-user', '/assign-quiz'];
+  // const studentPages = ['/dashboard', '/view-grades', '/submit-assignments', '/dashboard/attempt-quiz'];
+  // const managerPages = ['/dashboard', '/view-reports', '/manage-staff'];
+
+  const authRequired = !publicPages.includes(to.path);
+  const userRole = store.getters.getUserRole;
+
+  // If the route requires auth and there's no user role, redirect to login
+  if (authRequired && !userRole) {
+    return next('/login');
+  }
+
+  // If user has a role and tries to access a public page, redirect to dashboard
+  if (userRole && publicPages.includes(to.path)) {
+    return next('/dashboard');
+  }
+
+  // Role-based access control (commented out for now)
+  /*
+  if (userRole) {
+    if (userRole === 'admin' && !adminPages.includes(to.path) && !to.path.startsWith('/dashboard/')) {
+      return next('/dashboard');
+    } else if (userRole === 'student' && !studentPages.includes(to.path) && !to.path.startsWith('/dashboard/attempt-quiz')) {
+      return next('/dashboard');
+    } else if (userRole === 'manager' && !managerPages.includes(to.path) && !to.path.startsWith('/dashboard/')) {
+      return next('/dashboard');
+    }
+  }
+  */
+
+  // If none of the above conditions are met, allow navigation
+  next();
+};
+
+export default RouteGuard;
