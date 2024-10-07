d3fc8cdf8030c364aec077e96c1214dfa61e41e7
haroon-chh <haroon4501588@gmail.com>
2024-10-07 11:04:48 +0500

    dashboard setup done

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,21 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import DashboardView from '../views/DashboardView.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    redirect: '/dashboard',
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: '/dashboard',
+    name: 'dashboard',
+    component: DashboardView,
+  },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,18 @@
+<!-- src/views/DashboardView.vue -->
+<template>
+    <div class="dashboard-view">
+      <h1>Dashboard</h1>
+      <HelloWorld />
+    </div>
+  </template>
+  
+  <script setup>
+  import HelloWorld from '../components/dashboard/HelloWorldComponent.vue';
+  </script>
+  
+  <style scoped>
+  .dashboard-view {
+    padding: 20px;
+  }
+  </style>
+  
