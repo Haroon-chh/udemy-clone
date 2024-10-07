bab05f0d47d08bf2e1dd6d5a07e861e8dabdb29b
haroon-chh <haroon4501588@gmail.com>
2024-10-07 18:08:50 +0500

    done

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,46 @@
 <template>
   <div id="app">
-    <div class="navbar">  
-        <Navbar />
+    <!-- Navbar Section -->
+    <div class="navbar">
+      <Navbar />
     </div>
+
+    <!-- Main Content Section -->
     <div class="main-body">
       <router-view />
+    </div>
+
+    <!-- Footer Section (Sticky Footer) -->
+    <div class="footer">
+      <Footer />
     </div>
   </div>
 </template>
 
 <script setup>
 import Navbar from '../src/components/NavbarComponent.vue'; // Import the Navbar Component
+import Footer from '../src/components/Footer.vue';// Import the Footer Component
 </script>
 
 <style scoped>
-.navbar{
+.navbar {
   margin-bottom: 2%;
 }
+
 #app {
   font-family: Arial, sans-serif;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
 }
 
+.main-body {
+  flex: 1;
+}
 
+.footer {
+  text-align: center;
+  position: sticky;
+  width: 100%;
+}
 </style>
