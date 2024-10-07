20d1142c07e370268657d983d2460943a5dc7b22
haroon-chh <haroon4501588@gmail.com>
2024-10-07 11:09:55 +0500

    router sets

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import DashboardView from '../views/DashboardView.vue';
 
 const routes = [
@@ -14,7 +14,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,7 +1,6 @@
 <!-- src/views/DashboardView.vue -->
 <template>
     <div class="dashboard-view">
-      <h1>Dashboard</h1>
       <HelloWorld />
     </div>
   </template>
