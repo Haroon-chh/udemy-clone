51196530a594ccc717d5a3b757102bb032ac5fac
haroon-chh <haroon4501588@gmail.com>
2024-10-08 22:21:28 +0500

    route tittle done

--- src/router/index.js
+++ src/router/index.js
@@ -8,32 +8,45 @@ const routes = [
   {
     path: '/',
     redirect: '/dashboard',
+    
   },
   {
     path: '/dashboard',
     name: 'dashboard',
     component: DashboardView,
+    meta: { title: 'Dashboard'},
+
   },
   {
     path: '/signup',
     name: 'signup',
     component: SignupView,
+    meta: { title: 'SignUp'},
+
   },
   {
     path: '/about',
     name: 'about',
     component: AboutUsView,
+    meta: { title: 'About Us'},
+
   },
   {
     path: '/login',
     name: 'login',
     component: LoginView, // Add the Login route
+    meta: { title: 'Login'},
+
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || 'Udemy Clone';
+  next();
 });
 
 export default router;
