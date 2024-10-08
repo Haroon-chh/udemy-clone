b2ca0c3cd1b23bdcca8e166cb642fe99ed30439b
asmara-imtiaz <asmaraimtiaz0@gmail.com>
2024-10-08 17:20:27 +0500

    done

--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -2,13 +2,15 @@
 <template>
     <div class="about-view">
       <WelcomeComponent />
-      
+      <SkillComponent />
+
       
     </div>
   </template>
   
   <script setup>
-  import WelcomeComponent from '@/components/aboutUs/WelcomeComponent.vue';
+import WelcomeComponent from '@/components/aboutUs/WelcomeComponent.vue';
+import SkillComponent from '@/components/aboutUs/SkillComponent.vue';
  
   </script>
   
