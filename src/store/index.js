0d72f28a5c89120e12b93087fd8719ac7f0d507c
Muhammad Mudaser Khalid <Ranamudaser38@gmail.com>
2024-10-08 13:26:27 +0500

    done

--- src/store/index.js
+++ src/store/index.js
@@ -1,39 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import { createStore } from 'vuex'
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
+export default createStore({
+  state: {
   },
-  // Add more routes here
-];
-
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-});
-
-export default router;
-
-// src/store.js
-// src/store/index.js
-// src/store/index.js
-// src/store/index.js
-// import { createStore } from 'vuex';
-
-// const store = createStore({
-//   state: {
-//     items: [
-//       { id: 1, image: 'https://via.placeholder.com/800x400', title: 'Slide 1' },
-//       { id: 2, image: 'https://via.placeholder.com/800x400', title: 'Slide 2' },
-//       { id: 3, image: 'https://via.placeholder.com/800x400', title: 'Slide 3' },
-//     ],
-//   },
-//   getters: {
-//     getItems: (state) => state.items,
-//   },
-// });
-
-// export default store;
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
