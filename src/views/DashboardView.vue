f4a50c4589cee84afdede22aae08fdac853b0cf9
Ali-737 <syedaliahsan737@gmail.com>
2024-10-08 13:14:35 +0500

    FullStory component done by Ali

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -3,12 +3,14 @@
     <div class="dashboard-view">
       <HelloWorld />
       <LearningGoals />
+      <FullStory />
     </div>
   </template>
   
   <script setup>
   import HelloWorld from '../components/dashboard/HelloWorldComponent.vue';
   import LearningGoals from '@/components/dashboard/LearningGoals.vue';
+  import FullStory from '@/components/dashboard/FullStory.vue';
 
   </script>
   
