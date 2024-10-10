eeaa3dbc3b6c99c6e470736553601191d3f68190
asmara-imtiaz <asmaraimtiaz0@gmail.com>
2024-10-10 16:21:33 +0500

    done

---
+++ src/components/aboutUs/WorkCards.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="container my-5">
+      <div class="row text-center">
+       
+        <div class="col-md-4 ">
+          <div class="p-3">
+            <div class="underline-purple mb-3"></div>
+            <h4 class="fw-bold">Work with us</h4>
+            <p>At Udemy, we're all learners and instructors. We live out our values every day to create a culture that is diverse, inclusive, and committed to helping employees thrive.</p>
+            
+          </div>
+        </div>
+  
+        <!-- See our research -->
+        <div class="col-md-4">
+          <div class="p-3">
+            <div class="underline-purple mb-3"></div>
+            <h4 class="fw-bold">See our research</h4>
+            <p>We're committed to improving lives through learning. Dig into our original research to learn about the forces that are shaping the modern workplace.</p>
+            
+          </div>
+        </div>
+  
+        <!-- Read our blog -->
+        <div class="col-md-4">
+          <div class="p-3">
+            <div class="underline-teal mb-3"></div>
+            <h4 class="fw-bold">Read our blog</h4>
+            <p>Want to know what we've been up to lately? Check out the Udemy blog to get the scoop on the latest news, ideas, and projects.</p>
+            
+          </div>
+        </div>
+      </div>
+      <div class="row text-center">
+       
+       <div class="col-md-4">
+         <div class="p-3">
+            <a href="#" class="text-purple">Join our team &rarr;</a>
+        </div>
+        </div>
+
+        <div class="col-md-4">
+         <div class="p-3">
+            <a href="#" class="text-purple">Learn more &rarr;</a>
+        </div>
+        </div>
+
+        <div class="col-md-4">
+         <div class="p-3">
+            <a href="#" class="text-teal">Read now &rarr;</a>
+        </div>
+        </div>
+
+
+
+</div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "InfoComponent",
+  };
+  </script>
+  
+  <style>
+  .underline-purple {
+    height: 6px;
+    background-color: #7a1ba2;
+    margin: 0 auto;
+    width: max;
+  }
+  
+  .underline-teal {
+    height: 6px;
+    background-color: #008080;
+    width: max;
+    margin: 0 auto;
+  }
+  
+  .text-purple {
+    color: #7a1ba2;
+    text-align: left;
+  }
+  
+  .text-teal {
+    color: #008080;
+    text-align: left;
+
+  }
+  
+  h4 {
+    font-weight: 700;
+    margin-bottom: 30%;
+    text-align: left;
+  }
+  
+  p {
+    font-size: 1.2rem;
+    text-align: left;
+  }
+
+ 
+  </style>
+  
--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -5,6 +5,7 @@
       <SkillComponent />
       <CreatingImpact />
       <LearnMore />
+      <WorkCards/>
 
       
     </div>
@@ -15,6 +16,7 @@ import WelcomeComponent from '@/components/aboutUs/WelcomeComponent.vue';
 import SkillComponent from '@/components/aboutUs/SkillComponent.vue';
 import CreatingImpact from '@/components/aboutUs/CreatingImpact.vue';
 import LearnMore from '@/components/aboutUs/LearnMore.vue';
+import WorkCards from '@/components/aboutUs/WorkCards.vue';
  
   </script>
   
