a26578203ef2d649967d3dd82b03675a4fb572d1
asmara-imtiaz <asmaraimtiaz0@gmail.com>
2024-10-10 13:01:55 +0500

    hlaf about page done

--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -3,6 +3,8 @@
     <div class="about-view">
       <WelcomeComponent />
       <SkillComponent />
+      <CreatingImpact />
+      <LearnMore />
 
       
     </div>
@@ -11,6 +13,8 @@
   <script setup>
 import WelcomeComponent from '@/components/aboutUs/WelcomeComponent.vue';
 import SkillComponent from '@/components/aboutUs/SkillComponent.vue';
+import CreatingImpact from '@/components/aboutUs/CreatingImpact.vue';
+import LearnMore from '@/components/aboutUs/LearnMore.vue';
  
   </script>
   
