6d7ed179ddd613b0663fbb3f37ca0da050844359
Farhan ahmad <farhanahmid1@gmail.com>
2024-10-14 18:14:09 +0500

    done farhan

---
+++ src/views/SubscriptionView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <subscription />
+    </div>
+  </template>
+  
+  <script setup>
+  import subscription from '@/components/Profile/SubscriptionComponent.vue';
+  </script>
+  
+  <style scoped>
+  </style>
