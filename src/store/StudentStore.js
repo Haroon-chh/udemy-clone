75f20d372dccafabe4aa475c3aae5e4796a95935
Farhan ahmad <farhanahmid1@gmail.com>
2024-10-15 18:07:03 +0500

    done by farhan

---
+++ src/store/StudentStore.js
@@ -0,0 +1,14 @@
+// src/store/AdminStore.js
+import AuthApiServices from '@/services/AuthApiServices';
+import { toRaw } from 'vue'; // Import toRaw to convert reactive data to plain object
+
+const StudentStore = {
+  namespaced: true,
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {
+  },
+};
+
+export default StudentStore;
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from 'vuex';
 import AuthApiServices from '@/services/AuthApiServices'; // Import your Auth API services
 import AdminStore from './AdminStore';
+import StudentStore from './StudentStore';
 
 export default createStore({
   modules: {
-      admin:  AdminStore,
+      AdminStore,
+      StudentStore
   },
   state: {
     user: null,
