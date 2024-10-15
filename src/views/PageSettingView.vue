04062f3fe484f18dfc0fc50897139e835d8f567b
haroon-chh <haroon4501588@gmail.com>
2024-10-15 11:42:32 +0500

    setting page added

---
+++ src/views/PageSettingView.vue
@@ -0,0 +1,14 @@
+<!-- src/views/LoginView.vue -->
+<template>
+    <div class="page-view">
+      <PageSettingComponent />
+    </div>
+  </template>
+  
+  <script setup>
+  import PageSettingComponent from '../components/dashboard/Admin/PageSetting.vue';
+  </script>
+  
+  <style scoped>
+  </style>
+  
